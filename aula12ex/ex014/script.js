4688fc87b7a7eda5a974bbd0ea23738f1c15cb90
Marcio José  Da Silva Lima <67931783+marciojosedasilvalima@users.noreply.github.com>
2022-06-23 21:10:17 -0300

    troquei imagens de lugar

--- aula12ex/ex014/script.js
+++ aula12ex/ex014/script.js
@@ -3,18 +3,18 @@ function carregar() {
     var img = window.document.getElementById('imagem')
     var data = new Date()
     var hora = data.getHours()
- 
+    
     if (hora >= 0 && hora < 12) {
         msg.innerHTML = `Bom dia, agora são ${hora} hora(s).`
-        img.src = 'manha.jpg'
+        img.src = 'imagens/manha.jpg'
         window.document.body.style.background = '#e2cd9f'
     } else if (hora >= 12 && hora <= 18) {
         msg.innerHTML = `Boa tarde, agora são ${hora} horas.`
-        img.src = 'tarde.jpg'
+        img.src = 'imagens/tarde.jpg'
         window.document.body.style.background = '#b9846f'
     } else {
         msg.innerHTML = `Boa noite, agora são ${hora} horas.`
-        img.src = 'noite.jpg'
+        img.src = 'imagens/noite.jpg'
         window.document.body.style.background = '#515154'
     }
 }
