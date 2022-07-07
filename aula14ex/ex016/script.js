eae68c015838cf98a7eff754584322c0a87fe30d
Marcio José  Da Silva Lima <67931783+marciojosedasilvalima@users.noreply.github.com>
2022-07-06 22:50:08 -0300

    aulas

---
+++ aula14ex/ex016/script.js
@@ -0,0 +1,10 @@
+function contar() {
+    var ini = document.getElementById('numini')
+    var fim = document.getElementById('numfim')
+    var passo = document.getElementById('numpasso')
+}
+
+if (ini == '' !! fim == '') {
+    
+}
+
