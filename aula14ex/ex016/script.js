9c748c8d61a2ddbfc2f4a37ad2f50d4c589d55c9
Marcio José  Da Silva Lima <67931783+marciojosedasilvalima@users.noreply.github.com>
2022-08-02 12:57:50 -0300

    exercicio

--- aula14ex/ex016/script.js
+++ aula14ex/ex016/script.js
@@ -4,25 +4,25 @@ function contar() {
     var passo = document.getElementById('numpasso') 
     var resp  = document.querySelector('div#resp')   
 
-    
+   // var i = Number(ini.value)
+   
     if (ini.value.length == 0) {  
         resp.innerHTML = 'Impossível contar, Início deve ser preenchido.'
     } else if  (fim.value.length == 0) { 
         resp.innerHTML = 'Impossível contar, Fim deve ser preenchido.'
-    } else if (ini.value > fim.value) {
+    } else if (Number(ini.value) > Number(fim.value)) {
         resp.innerHTML = 'Início não pode ser maior que o fim.'
     } else {
-        if (passo.value.length == 0) { 
-            window.alert('Passo inválido! Considerando PASSO 1.')        
+        if (passo.value.length == 0 || passo.value == 0) { 
+            window.alert('Passo inválido! Considerando PASSO 1.')
             passo.value = 1
-        } 
-
-        resp.innerHTML = 'Teste'
-        
- /*
-        for(c.value; c.value <= y.value; c.value = Number(c.value) + Number(p.value)) {
-            resp.innerHTML = c.value            
-        }                
-   */ 
+        }
+ 
+        resp.innerHTML = ``
+        for(i = Number(ini.value); i <= Number(fim.value); i = i + Number(passo.value)) {            
+            resp.innerHTML += `${i} &#x1F449 `
+        }  
+        resp.innerHTML += `&#x1F3C1`             
+    
     } 
 }
